e5b87b0a6cdd10f929ef9a42ebc32f8fb40ac9ac
othree <othree@gmail.com>
2010-01-05 20:29:15 +0800

    Add bbs/

---
+++ bbs/archive-irclogs.sh
@@ -0,0 +1,5 @@
+
+Y=`date +%Y`
+Y=`expr $Y - 1`
+
+#tar -czf ~/irclogs-archive/irclogs-$Y.tar.gz ~/irclogs/
---
+++ bbs/bbs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export TERM=xterm
+export LC_CTYPE=big5
+export LANG=zh_TW.big5
+~/bbs/bbs.exp $*
